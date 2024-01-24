4b6701278561532dc4aee0b1c37efaa380f8b51e
dpsi9 <dipendras953@gmail.com>
2024-01-24 23:56:02 +0530

    setup database

--- api/index.js
+++ api/index.js
@@ -1,11 +1,18 @@
 import express from "express";
-
+import connectDB from "./db/index.js";
 const app = express();
 
 app.get("/",(req,res)=>{
     res.send("Hello from api")
 })
 
-app.listen(3000,() => {
-    console.log(`server is listening on port 3000`);
+connectDB()
+.then(() => {
+    app.listen(3000,() => {
+        console.log(`server is listening on port 3000`);
+    })
 })
+.catch((error) =>{
+    console.log("MongoDB failed to connect",error);
+})
+
